9a55c28cfa62757bd58462d20bdcc2febf9d3272
Gvozden Kuzman <gvozden.kuzman@nederlandseloterij.nl>
2024-04-16 23:01:43 +0200

    add silo

---
+++ Silo/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var host = new HostBuilder()
+  .UseOrleans(ConfigureSilo)
+  .ConfigureLogging(logging => logging.AddConsole())
+  .Build();
+await host.RunAsync();
+
+return 0;
+
+static void ConfigureSilo(HostBuilderContext context, ISiloBuilder siloBuilder)
+{
+  siloBuilder.UseAzureStorageClustering(options => options.ConfigureTableServiceClient("UseDevelopmentStorage=true"));
+}
